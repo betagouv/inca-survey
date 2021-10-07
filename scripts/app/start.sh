5e7fa8b9c2ca6975a6a544bf09cc53112f7e59ac
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-07 04:53:16 +0200

    ci(traefik): bind survey.lab-agora.fr

--- scripts/app/start.sh
+++ scripts/app/start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Exit when any command fails
 set -e
@@ -7,4 +7,4 @@ echo "Stopping existing Docker containers..."
 docker-compose down
 
 echo "Starting db and app Docker containers..."
-docker-compose up -d app
+docker-compose up -d survey_app
