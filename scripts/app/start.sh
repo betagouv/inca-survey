153b17747cb9b5d3e03745f0a14a2689476a3cdd
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-07 12:58:58 +0200

    ci(scripts): rebuild survey_app container before each start

--- scripts/app/start.sh
+++ scripts/app/start.sh
@@ -3,8 +3,11 @@
 # Exit when any command fails
 set -e
 
+echo "Building survey_app Docker container..."
+sudo docker-compose build --no-cache --pull survey_app
+
 echo "Stopping existing Docker containers..."
 docker-compose down
 
-echo "Starting db and app Docker containers..."
+echo "Starting survey_db and survey_app Docker containers..."
 docker-compose up -d survey_app
